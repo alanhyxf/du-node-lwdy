e858503bc846dbd0ec62705c977ad25657407415
2334769916 <2334769916@qq.com>
2018-10-29 00:58:01 +0800

    7

--- Bot.js
+++ Bot.js
@@ -85,44 +85,31 @@ class InquiryBot extends Bot {
                 var key=results[i].content;
                 var obj={};
                 obj[key]=['a','b','c'];
-		questionsList.push(obj);
+		        questionsList.push(obj);
 
-            }
-        });
+                let gameQuestions = this.populateGameQuestions(questionsList);
+                let correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
+                console.log(correctAnswerIndex);
+                let roundAnswers = this.populateRoundAnswers(gameQuestions, 0,correctAnswerIndex,questionsList);
+                let currentQuestionIndex = 0;
+                let spokenQuestion = Object.keys(questionsList[gameQuestions[currentQuestionIndex]])[0];
+                let repromptText = '第1题：\n' + spokenQuestion + '\n';
+                for (let i = 0; i < ANSWER_COUNT; i += 1) {
+                    repromptText += `${i + 1}. ${roundAnswers[i]}. `;
+                }
 
+                let currentQuestion = questionsList[gameQuestions[currentQuestionIndex]];
 
+                this.setSessionAttribute('currentQuestionIndex',currentQuestionIndex);
+                this.setSessionAttribute('correctAnswerIndex',correctAnswerIndex + 1);
+                this.setSessionAttribute('gameQuestions',gameQuestions);
+                this.setSessionAttribute('questionsList',questionsList);
+                this.setSessionAttribute('score',0);
+                this.setSessionAttribute('correctAnswerText',currentQuestion[Object.keys(currentQuestion)[0]][0]);
+                return repromptText;
+                    }
+                });
 
-
-
-
-
-    //    let questionsList = questions.QUESTIONS;
-      //     for(var i = 0; i < questionsList.length; i++)
-      //      {
-      //          let Qes=questionsList[i];
-      //          console.log("%s\t%s", Object.keys(Qes),Qes[Object.keys(Qes)[0]].slice());
-
-      //      } 
-        let gameQuestions = this.populateGameQuestions(questionsList);
-        let correctAnswerIndex = Math.floor(Math.random() * (ANSWER_COUNT));
-        console.log(correctAnswerIndex);
-        let roundAnswers = this.populateRoundAnswers(gameQuestions, 0,correctAnswerIndex,questionsList);
-        let currentQuestionIndex = 0;
-        let spokenQuestion = Object.keys(questionsList[gameQuestions[currentQuestionIndex]])[0];
-        let repromptText = '第1题：\n' + spokenQuestion + '\n';
-        for (let i = 0; i < ANSWER_COUNT; i += 1) {
-            repromptText += `${i + 1}. ${roundAnswers[i]}. `;
-        }
-
-        let currentQuestion = questionsList[gameQuestions[currentQuestionIndex]];
-
-        this.setSessionAttribute('currentQuestionIndex',currentQuestionIndex);
-        this.setSessionAttribute('correctAnswerIndex',correctAnswerIndex + 1);
-        this.setSessionAttribute('gameQuestions',gameQuestions);
-        this.setSessionAttribute('questionsList',questionsList);
-        this.setSessionAttribute('score',0);
-        this.setSessionAttribute('correctAnswerText',currentQuestion[Object.keys(currentQuestion)[0]][0]);
-        return repromptText;
     }
 
   /**
